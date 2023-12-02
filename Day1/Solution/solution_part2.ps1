db49de877c634c7c9d1954f3283a4557bb07660c
Damien <admin@ddelay.co.uk>
2023-12-02 18:23:58 +0000

    Re-added Day1
    Fixed Day 1 being missing.

---
+++ Day1/Solution/solution_part2.ps1
@@ -0,0 +1,54 @@
+$DebugPreference = 'SilentlyContinue'
+
+function ConvertString-ToNumberFormat {
+    param (
+        [String]$textString
+    )
+
+    $replacementString = $textString
+
+    $numberWordMappings = @{
+        "one" = "o1e"
+        "two" = "t2o"
+        "three" = "t3e"
+        "four" = "f4r"
+        "five" = "f5e"
+        "six" = "s6x"
+        "seven" = "s7n"
+        "eight" = "e8t"
+        "nine" = "n9e"
+    }
+
+    foreach($key in $numberWordMappings.Keys){
+        $replacementString = $replacementString -replace $key, $numberWordMappings[$key]
+    }
+    return $replacementString
+        
+}
+
+$muddledArray = Get-Content -Path ../puzzle_input.txt
+
+$regex = '[^\d]'
+
+$calibrationObjects = New-Object System.Collections.ArrayList
+
+foreach($muddledString in $muddledArray){
+    Write-Debug "Processing: $muddledString"
+    $convertedString = ConvertString-ToNumberFormat $muddledString
+
+    $numbers = $convertedString -replace $regex, ''
+
+   $calibrationObject = [PSCustomObject]@{
+       orginalString = $muddledString
+       firstDigit = $numbers[0]
+       lastDigit    = $numbers[-1]
+       calibrationValue = [String]::concat($numbers[0], $numbers[-1])
+   }
+
+   Write-Debug "First Digit: $($calibrationObject.firstDigit), Last Digit: $($calibrationObject.lastDigit), Calibration Value: $($calibrationObject.calibrationValue)"
+   $calibrationObjects.Add($calibrationObject) | Out-Null
+}
+
+$sumValue = ($calibrationObjects | Measure-Object -Property calibrationValue -Sum).Sum
+
+Write-Host "Sum Value is: $sumValue"
